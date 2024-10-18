e2ca7926c2734dbb4640836dce7992cb8b274b75
JW011 <145181089+JW011@users.noreply.github.com>
2024-10-19 02:14:16 +0530

    Circular Timer
    Value Changes from 100 to 0, compare using "$TextureProgressBar.value"

---
+++ scripts/meter.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	$TextureProgressBar.value-=1
+	
+	
+	
+	
