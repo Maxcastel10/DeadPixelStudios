8b96820a550968ff518d0350be7c9cd815d1043b
CactusAaron05 <aaronjdias2005@gmail.com>
2024-10-19 02:48:54 +0530

    volume sliders

--- scene/option_menu.gd
+++ scene/option_menu.gd
@@ -13,3 +13,9 @@ func _process(delta: float) -> void:
 
 func _on_return_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/menu.tscn")
+
+
+func _on_apply_pressed() -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db($Audio_options/VBoxContainer/Master_slider.value))
+	AudioServer.set_bus_volume_db(1, linear_to_db($Audio_options/VBoxContainer/Music_slider.value))
+	AudioServer.set_bus_volume_db(2, linear_to_db($Audio_options/VBoxContainer/SFX_slider.value))
